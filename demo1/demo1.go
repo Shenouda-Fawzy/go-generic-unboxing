97290e74219af6e0aee86ff65b0c93105a549d9c
Shenouda Fawzy <Shenouda.Fawzy@hotmail.com>
2022-01-24 21:41:56 +0200

    Pumbing go.mod to go1.18 + split demos

---
+++ demo1/demo1.go
@@ -0,0 +1,28 @@
+package demo1
+
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+// SumInts adds together the values of m.
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumFloats adds together the values of m.
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github/go-generic/demo1"
+)
 
-// SumInts adds together the values of m.
-func SumInts(m map[string]int64) int64 {
-	var s int64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-// SumFloats adds together the values of m.
-func SumFloats(m map[string]float64) float64 {
-	var s float64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
 
 func main() {
 	// Initialize a map for the integer values
@@ -34,18 +20,11 @@ func main() {
 	}
 
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
-		SumInts(ints),
-		SumFloats(floats))
+		demo1.SumInts(ints),
+		demo1.SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats[string, int64](ints),
-		SumIntsOrFloats[string, float64](floats))
+		demo1.SumIntsOrFloats[string, int64](ints),
+		demo1.SumIntsOrFloats[string, float64](floats))
 }
 
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
-    var s V
-    for _, v := range m {
-        s += v
-    }
-    return s
-}
